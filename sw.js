fbb09a3eef67933616bc437f0dea19b6383341fa
Kevin Correa García <99368615+KevinCGarx@users.noreply.github.com>
2026-09-24 03:23:59 -0600

    Update CACHE_VERSION to v1.3

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Bump this version string any time you change index.html (or any cached file)
 // so returning visits pick up the new version instead of a stale cached copy.
-const CACHE_VERSION = 'salud.familiar-v1.2';
+const CACHE_VERSION = 'salud.familiar-v1.3';
 const APP_SHELL = [
   './',
   './index.html',
